20ea2292009f83dcb832c83e0bd85ca372698828
Danny Brown <machinagod@googlemail.com>
2020-04-27 19:53:01 +0100

    logging for authentication

--- Andgasm.HoundDog/Andgasm.HoundDog.AccountManagement.Core/AuthManagement/UserAuthenticationManager.cs
+++ Andgasm.HoundDog/Andgasm.HoundDog.AccountManagement.Core/AuthManagement/UserAuthenticationManager.cs
@@ -37,8 +37,9 @@ namespace Andgasm.HoundDog.AccountManagement.Core.AuthManagement
         #region Authentication
         public async Task<(UserDTO ValidatedUser, FieldValidationErrorDTO Error)> AuthenticateUserCredentials(string username, string password, string verificationCode)
         {
+            _logger.LogDebug($"Authenticating for user {username}");
             var user = await FindUserForIdentifier(username);
-            if (user == null) return (null, new FieldValidationErrorDTO(nameof(UserSignInDTO.SuppliedUserName), "Specified user does not exist!"));
+            if (user == null) return UserDoesNotExist(username);
 
             var signinresult = await _signinManager.PasswordSignInAsync(user, password, true, false);
             if (signinresult.Succeeded)
@@ -49,29 +50,55 @@ namespace Andgasm.HoundDog.AccountManagement.Core.AuthManagement
             else if (signinresult.RequiresTwoFactor)
             {
                 var tfaresult = await _userauthManager.ConfirmAuthenticatorCode(user.Id, verificationCode);
-                if (!tfaresult.Succeeded) return (null, new FieldValidationErrorDTO(nameof(UserSignInDTO.VerificationCode), "Your authentication code could not be validated!"));
+                if (!tfaresult.Succeeded) return Failed2FAChallenge(verificationCode, user.Id);
 
-                var userdto = await MapUserToDTOWithRoles(user);                
+                var userdto = await MapUserToDTOWithRoles(user);
                 return (userdto, null);
             }
-            else if (signinresult.IsLockedOut) return (null, new FieldValidationErrorDTO(nameof(UserSignInDTO.SuppliedPassword), "This account is locked!"));
-            else 
-            {
-                return (null, new FieldValidationErrorDTO(nameof(UserSignInDTO.SuppliedPassword), "Specified password is not correct for user!"));
-            }
+            else if (signinresult.IsLockedOut) return AccountLockedOut(user.Id);
+            else return AuthenticationFailed(user.Id);
         }
 
         public async Task<bool> UsernameRequires2FAVerification(string useridentifier)
         {
+            _logger.LogDebug($"Determining 2FA status for user {useridentifier}");
             var user = await FindUserForIdentifier(useridentifier);
             if (user == null) return false;
             return user.TwoFactorEnabled;
+        }
+
+        #endregion
+
+        #region Error Responses
+        private (UserDTO ValidatedUser, FieldValidationErrorDTO Error) UserDoesNotExist(string username)
+        {
+            _logger.LogError($"Specified username {username} does not exist!");
+            return (null, new FieldValidationErrorDTO(nameof(UserSignInDTO.SuppliedUserName), "Specified user does not exist!"));
+        }
+
+        private (UserDTO ValidatedUser, FieldValidationErrorDTO Error) Failed2FAChallenge(string code, string userid)
+        {
+            _logger.LogError($"The authentication code {code} could not be validated for user {userid}!");
+            return (null, new FieldValidationErrorDTO(nameof(UserSignInDTO.VerificationCode), "Your authentication code could not be validated!"));
+        }
+
+        private (UserDTO ValidatedUser, FieldValidationErrorDTO Error) AccountLockedOut(string userid)
+        {
+            _logger.LogError($"Attempt to sign in to account for user {userid} is locked!!");
+            return (null, new FieldValidationErrorDTO(nameof(UserSignInDTO.SuppliedUserName), "This account is locked!"));
+        }
+
+        private (UserDTO ValidatedUser, FieldValidationErrorDTO Error) AuthenticationFailed(string userid)
+        {
+            _logger.LogError($"Attempt to sign in to account for user {userid} failed!!");
+            return (null, new FieldValidationErrorDTO(nameof(UserSignInDTO.SuppliedPassword), "Specified password is not correct for user!"));
         }
         #endregion
 
         #region Mappings
         private async Task<UserDTO> MapUserToDTOWithRoles(HoundDogUser user)
         {
+            _logger.LogDebug($"Mapping db user {user.Id} to dto user");
             var userdto = _mapper.Map<UserDTO>(user);
             userdto.Roles = string.Join(", ", await _signinManager.UserManager.GetRolesAsync(user));
             return userdto;
@@ -79,6 +106,7 @@ namespace Andgasm.HoundDog.AccountManagement.Core.AuthManagement
 
         private async Task<HoundDogUser> FindUserForIdentifier(string useridentifier)
         {
+            _logger.LogDebug($"Finding user by id {useridentifier}");
             var user = await _signinManager.UserManager.FindByNameAsync(useridentifier);
             if (user == null) user = await _signinManager.UserManager.FindByEmailAsync(useridentifier);
             if (user == null) user = await _signinManager.UserManager.FindByIdAsync(useridentifier);
