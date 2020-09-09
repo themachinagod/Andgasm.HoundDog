3cf90839e9fc826838f63805c8fb78e3b5f686c6
Danny Brown <machinagod@googlemail.com>
2020-09-09 16:42:42 +0100

    deletion of old web project

--- Andgasm.HoundDog/Andgasm.HoundDog.AngularWeb/ClientApp/src/app/shared/models/user.registration.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface UserRegistration {
-  email: string;
-  password: string;
-  confirmPassword: string;
-  firstName: string;
-  lastName: string;
-  userName: string;
-  location: string;
-}
